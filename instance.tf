94411804df6d74d5a494d9050e427a57dd3d3617
Miguel Vega <Miguel.A.Vega@live.com>
2020-04-12 19:53:00 -0400

    Initial commit.

---
+++ instance.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "example" {
+    ami                     = "ami-0100cc5882285d784"
+    instance_type           = "t2.micro"
+    vpc_security_group_ids  = [aws_security_group.example.id]
+
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p ${var.server_port} &
+                EOF
+
+    tags = {
+        Name = "terraform-example"
+    }
+}
---
+++ security_group.tf
@@ -0,0 +1,10 @@
+resource aws_security_group "example" {
+    name = "security-group-example"
+
+    ingress {
+        from_port   = var.server_port
+        to_port     = var.server_port
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
