c12a2047c2a9583b60fb070817b63a4bde58d41c
Miguel Vega <Miguel.A.Vega@live.com>
2020-07-09 17:20:08 -0400

    Added security group TF script. Change instance TF script to configure busybox as part of the user_data bootstrapping. Configured EC2 to use the newly created security group as well.

--- Chapter 2/instance.tf
+++ Chapter 2/instance.tf
@@ -1,8 +1,15 @@
 resource "aws_instance" "example" {
-    ami             = "ami-0100cc5882285d784"
-    instance_type   = "t2.micro"
+    ami                     = "ami-0100cc5882285d784"
+    instance_type           = "t2.micro"
+    vpc_security_group_ids  = [aws_security_group.instance.id]
+
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
 
     tags = {
-        Name    = "Chapter 2"
+        Name    = "terraform-example"
     }
 }
