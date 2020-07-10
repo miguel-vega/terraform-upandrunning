1f7b478064404bdab2c84e27a633983acc705aa0
Miguel Vega <Miguel.A.Vega@live.com>
2020-07-10 18:11:27 -0400

    Commented out public_ip output variable.

--- Chapter 2/variables.tf
+++ Chapter 2/variables.tf
@@ -4,10 +4,10 @@ variable "server_port" {
     default     = 8080
 }
 
-output "public_ip" {
-    value       = aws_instance.example.public_ip
-    description = "The public IP address of the web server"
-}
+#output "public_ip" {
+#    value       = aws_instance.example.public_ip
+#    description = "The public IP address of the web server"
+#}
 
 output "alb_dns_name" {
     value       = aws_lb.example.dns_name
